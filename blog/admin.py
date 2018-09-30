b08b4110c57281882c8f55402cd9c69611901224
Williano <paawilly17@gmail.com>
2018-09-30 16:35:47 +0000

    [ADD] :sparkles: Add models for blog app
    Users can now create categories, articles and add comments to an
    article at the admin backend

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,110 @@ from django.contrib import admin
 
 # Local application imports.
 from .models.author_profile import Profile
+from .models.blog import Category, Article, Comment
 
 
-# Registers the author profile at the admin backend.
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+     Customize how the profile model is displayed in the admin site.
+
+     List display shows how the profile fields will be displayed.
+
+     List filter shows how the profiles can be filtered.
+
+     Search fields allows users to search profiles using user names.
+
+     Ordering orders the profiles with the users.
+    """
+    list_display = ('user', 'image',)
+    list_filter = ('user',)
+    search_fields = ('user',)
+    ordering = ['user', ]
+
+
+# Registers the author profile model at the admin backend.
+admin.site.register(Profile, ProfileAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+     Customize how the category model is displayed in the admin site.
+
+     List display shows how the category fields will be displayed.
+
+     List filter shows how the category can be filtered.
+
+     Search fields allows users to search categories using category names.
+
+     Ordering orders the categories with their names.
+    """
+    list_display = ('name', 'slug', 'image')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['name', ]
+
+
+# Registers the category model at the admin backend.
+admin.site.register(Category, CategoryAdmin)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    """
+     Customize how the article model is displayed in the admin site.
+
+     List display shows how the article fields will be displayed.
+
+     List filter shows how the article can be filtered.
+
+     Search fields allows users to search articles using article title.
+
+     Prepopulated fields fills the slug field automatically with the title of
+     the article when the user is typing the article title.
+
+    raw_id_field allows the author field to be displayed with a lookup widget
+    that can scale much better than a dropdown select input when you
+    have thousands of users
+
+    Date hierarchy allows users to search articles based on the published date.
+
+     Ordering orders the articles with their titles.
+    """
+    list_display = ('category', 'title', 'slug', 'author', 'image',
+                    'body', 'date_published', 'status')
+    list_filter = ('status', 'date_created', 'date_published', 'author',)
+    search_fields = ('title', 'body',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'date_published'
+    ordering = ['status', 'date_published', ]
+
+
+# Registers the article model at the admin backend.
+admin.site.register(Article, ArticleAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+     Customize how the article model is displayed in the admin site.
+
+     List display shows how the article fields will be displayed.
+
+     List filter shows how the article can be filtered.
+
+     Search fields allows users to search articles using article title.
+
+    Date hierarchy allows users to search articles based on the published date.
+
+     Ordering orders the articles with their titles.
+    """
+    list_display = ('name', 'email', 'comment', 'article', 'date_created',
+                    'approved')
+    list_filter = ('approved', 'date_created', 'name',)
+    search_fields = ('name', 'article', 'comment')
+    date_hierarchy = 'date_created'
+    ordering = ['approved', 'date_created', ]
+
+
+# Registers the comment model at the admin backend.
+admin.site.register(Comment, CommentAdmin)
---
+++ blog/models/blog.py
@@ -0,0 +1,138 @@
+# Third party imports.
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.text import slugify
+from taggit.managers import TaggableManager
+
+
+class Category(models.Model):
+    """
+     Create a category model
+    """
+    name = models.CharField(max_length=100, blank=False)
+    slug = models.SlugField()
+    image = models.ImageField(default='default.jpg',
+                              upload_to='category/category_images')
+
+    class Meta:
+        """
+        Specifies various model-specific options for the Category Model.
+        """
+        unique_together = ('name', 'slug')
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        """
+        Returns a human-readable representation of the Category Model.
+
+        """
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the default save method and generate the slug field
+        automatically when a category is saved.
+
+        Allows unicode in slug field. Ex: If value is category name is
+        "&* World", the output will be "&*-world".
+
+        """
+        self.slug = slugify(self.name, allow_unicode=True)
+        super(Category, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of category.
+
+        """
+        return reverse('blog:articles_list_by_category',
+                       kwargs={'slug': self.slug})
+
+
+class Article(models.Model):
+    """
+      Create an article model.
+    """
+    # CHOICES
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='articles')
+    title = models.CharField(max_length=250, blank=False)
+    slug = models.SlugField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='articles')
+    image = models.ImageField(default='default.jpg',
+                              upload_to='article/article_pics')
+    body = models.TextField(blank=False)
+    tags = TaggableManager()
+    date_published = models.DateTimeField(default=timezone.now)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='draft')
+
+    class Meta:
+        """
+        Specifies various model-specific options for the Article Model.
+        """
+        unique_together = ("title", "slug",)
+        ordering = ('-date_published',)
+
+    def __str__(self):
+        """
+        Returns a human-readable representation of the blog model.
+        """
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the default save method and generate the slug field
+        automatically when a category is saved.
+
+        Allows unicode in slug field. Ex: If value is category name is
+        "&* World", the output will be "&*-world".
+
+        """
+        self.slug = slugify(self.title, allow_unicode=True)
+        super(Article, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular instance of article.
+
+        """
+        return reverse('blog:blog_detail', kwargs={'slug': self.slug})
+
+
+class Comment(models.Model):
+    """
+      Create a Comment model.
+    """
+    name = models.CharField(max_length=250, blank=False)
+    email = models.EmailField()
+    comment = models.TextField(blank=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,
+                                related_name='comments')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """
+        Specifies various model-specific options for the Comment Model.
+        """
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        """
+        Returns a human-readable representation of the Comment Model.
+
+        """
+        return f"Comment by {self.name} on {self.article}"
