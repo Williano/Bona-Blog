b123f15a595096f3b1a0eba68ee54df513d23142
Williano <paawilly17@gmail.com>
2018-10-07 15:49:42 +0000

    [UPDATE] :zap: Update image upload to path

--- blog/models/author_profile_model.py
+++ blog/models/author_profile_model.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',
-                              upload_to='author/profile_pics')
+                              upload_to='profile_pics')
 
     def __str__(self):
         """
