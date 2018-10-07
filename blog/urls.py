6cfc97e4a98fcbb0a84b70e8dd483319f5261f59
Williano <paawilly17@gmail.com>
2018-10-07 15:48:01 +0000

    [WORK IN PROGRESS] :construction: Work on homepage view to list articles

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,20 @@
+"""
+ blog/urls.py
+"""
+
 # Third party imports.
 from django.urls import path
 
+# Local application imports.
+from blog.views.blog_view import (
+    ArticleListView,
+)
 
 # Specifies the app name for name spacing.
 app_name = "blog"
 
 urlpatterns = [
+    # /blog/home/
+    path('', ArticleListView.as_view(), name='home'),
 
 ]
--- blog/views/blog_view.py
+++ blog/views/blog_view.py
@@ -1,3 +1,32 @@
 # Third party imports.
-from django.contrib.auth.models import User
-from django.db import models
+from django.views.generic import ListView
+
+# Local application imports.
+from blog.models.blog_model import Category, Article, Comment
+
+
+class ArticleListView(ListView):
+    """
+     This view list all articles and categories.
+    """
+    template_name = "blog/home.html"
+
+    def get_queryset(self):
+        """
+        Gets all articles which are published.
+
+        :return: dictionary: a dictionary of all articles and their ids
+        """
+        articles = Article.objects.filter(status="Published")
+        return articles
+
+    def get_context_data(self, **kwargs):
+        """
+        Calls the base implementation first to get a context
+
+        :return: dictionary: a dictionary of all categories and their ids
+        """
+        context = super().get_context_data(**kwargs)
+        # Get all blog categories and return it to the view.
+        context['categories'] = Category.objects.all()
+        return context
