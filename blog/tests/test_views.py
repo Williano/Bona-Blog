eb9065bbcfb1dc49a27a613ead61170580949e9a
Williano <paawilly17@gmail.com>
2018-11-10 19:23:48 +0000

    [TEST] :rotating_light: Add more test to category articles list view

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -200,7 +200,7 @@ class AuthorsListViewTests(TestCase):
                          self.authors[0].image)
 
 
-class CategoryArticleListViewTest(TestCase):
+class CategoryArticlesListViewTest(TestCase):
     """
     Class to test a particular category's articles.
     """
@@ -211,6 +211,7 @@ class CategoryArticleListViewTest(TestCase):
         """
         self.client = Client()
         self.category = mommy.make(Category)
+        self.articles = mommy.make(Article, category=self.category, _quantity=5)
 
     def test_category_article_list_view_status_code(self):
         response = self.client.get(self.category.get_absolute_url())
@@ -226,8 +227,57 @@ class CategoryArticleListViewTest(TestCase):
                                            kwargs={'slug': self.category.slug}))
         self.assertTemplateUsed(response, 'blog/category_articles.html')
 
+    def test_if_category_articles_list_view_returns_the_right_number_of_articles(self):
+        response = self.client.get(reverse('blog:category_articles',
+                                           kwargs={'slug': self.category.slug}))
+        self.assertEqual(len(response.context_data["articles"]), 5)
 
-class AuthorArticleListViewTest(TestCase):
+    def test_if_category_articles_list_view_returns_the_right_category_details(self):
+        response = self.client.get(self.category.get_absolute_url())
+        self.assertEqual(response.context_data["articles"][0].category,
+                         self.category)
+        self.assertEqual(response.context_data["articles"][0].category.name,
+                         self.category.name)
+        self.assertEqual(response.context_data["articles"][0].category.slug,
+                         self.category.slug)
+        self.assertEqual(response.context_data["articles"][0].category.image,
+                         self.category.image)
+
+    def test_if_category_articles_list_view_returns_the_right_article_details(self):
+        """
+        This test checks if the view returns the right articles according to the
+        date they were published.
+
+        In the setup, model mommy creates five articles and store
+        them in a list called articles. So the last article in the list will
+        be the first article in the list view since it was created last by model
+        mommy.
+        The list view orders articles according to the time they were published
+        so the last article in the articles list will be displayed first in the
+        view.
+        """
+        response = self.client.get(self.category.get_absolute_url())
+        self.assertEqual(response.context_data['articles'][0].category,
+                         self.articles[4].category)
+        self.assertEqual(response.context_data['articles'][0].title,
+                         self.articles[4].title)
+        self.assertEqual(response.context_data['articles'][0].slug,
+                         self.articles[4].slug)
+        self.assertEqual(response.context_data['articles'][0].author,
+                         self.articles[4].author)
+        self.assertEqual(response.context_data['articles'][0].image,
+                         self.articles[4].image)
+        self.assertEqual(response.context_data['articles'][0].body,
+                         self.articles[4].body)
+        self.assertEqual(response.context_data['articles'][0].date_published,
+                         self.articles[4].date_published)
+        self.assertEqual(response.context_data['articles'][0].date_created,
+                         self.articles[4].date_created)
+        self.assertEqual(response.context_data['articles'][0].status,
+                         self.articles[4].status)
+
+
+class AuthorArticlesListViewTest(TestCase):
     """
       Class to test a particular author's articles.
     """
