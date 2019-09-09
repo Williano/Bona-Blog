542ffdff35b316dfdb7beed863af135d8dbc41e4
Williano <paawilly17@gmail.com>
2019-09-09 16:37:03 +0000

    [UPDATE] :zap: Add email settings

--- bona_blog/settings.py
+++ bona_blog/settings.py
@@ -183,10 +183,15 @@ LOGIN_URL = '/account/login/'
 LOGIN_REDIRECT_URL = '/author/dashboard/'
 LOGOUT_REDIRECT_URL = '/account/login/'
 
-# Email Settings
+# Email Settings (Development)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = ''
-# EMAIL_PORT = 587
+
+
+# Email Settings (Production)
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
 # EMAIL_HOST_USER = ''
-# EMAIL_HOST_PASSWORD = ''
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+
