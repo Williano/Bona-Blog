7f4fafa6e7d76362391e302b57df8bf89c7baec5
Williano <paawilly17@gmail.com>
2019-09-09 03:06:11 +0000

    [UPDATE] :zap: Remove autofocus from username

--- blog/forms/account/register_forms.py
+++ blog/forms/account/register_forms.py
@@ -1,27 +1,3 @@
-# # Django imports
-# from django import forms
-#
-#
-# class UserRegisterForm(forms.Form):
-#     """
-#         Creates User registration form for signing up.
-#     """
-#
-#     username = forms.CharField(widget=forms.TextInput(attrs={
-#             "name": "username", "class": "input100",
-#             "placeholder": "Username"
-#         }))
-#
-#     email = forms.EmailField(widget=forms.EmailInput(attrs={
-#             "name": "email", "class": "input100",
-#             "placeholder": "Email"
-#         }))
-#
-#     password = forms.CharField(widget=forms.PasswordInput(attrs={
-#             "name": "password",  "class": "input100",
-#             "placeholder": "Password"
-#         }))
-
 # Django imports
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
