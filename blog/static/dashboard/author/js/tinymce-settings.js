d14c309ccd0bcbc114fd45615a773a24b31d6c3b
Williano <paawilly17@gmail.com>
2020-06-26 04:50:10 -0500

    Update

--- blog/static/dashboard/author/js/tinymce-settings.js
+++ blog/static/dashboard/author/js/tinymce-settings.js
@@ -4,16 +4,16 @@
       max_height: 700,
       min_height: 500,
       min_width: 400,
-      plugins: 'autolink lists media table' +
+      plugins: 'autolink lists media table  ' +
           'emoticons anchor pagebreak visualchars code ' +
-          'fullscreen autolink lists  charmap print  hr contextmenu directionality ' +
-          'searchreplace wordcount visualblocks table lists fullscreen  ' +
-          'insertdatetime  nonbreaking textcolor save link image media preview ' +
-          'codesample contextmenu',
-      toolbar1: 'checklist code formatpainter pageembed permanentpen table fullscreen preview bold ' +
+          'fullscreen autolink lists  charmap print  hr searchreplace wordcount visualblocks table lists fullscreen  ' +
+          'insertdatetime  nonbreaking save link image media preview ' +
+          'codesample',
+      toolbar1: 'checklist table fullscreen preview bold ' +
                 'italic underline fontselect fontsizeselect forecolor backcolor ' +
                 'alignleft alignright aligncenter alignjustify indent outdent ' +
-                'bullist numlist table link image media codesample emoticons visualblocks ' +
+                'bullist numlist link image media codesample emoticons visualblocks ' +
                 'visualchars charmap hr pagebreak nonbreaking anchor code ',
       toolbar_mode: 'floating',
+      custom_undo_redo_levels: 20,
     });
