05b553a9a128528a2d0ffb631cbd2fb783b38596
Williano <paawilly17@gmail.com>
2021-02-19 22:28:55 -0600

    Fix article image not showing

--- blog/views/dashboard/author/dashboard_views.py
+++ blog/views/dashboard/author/dashboard_views.py
@@ -62,7 +62,8 @@ class ArticleWriteView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
 
-        article_create_form = ArticleCreateForm(request.POST)
+        article_create_form = ArticleCreateForm(request.POST, request.FILES)
+        print(request.FILES)
 
         action = request.POST.get("action")
         article_status = request.POST["status"]
