531dc92a622334852ff810c670421fb076b43c44
Williano <paawilly17@gmail.com>
2021-04-25 00:43:01 -0500

    Update

--- blog/apps.py
+++ blog/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
     name = 'blog'
 
     # Starts signals to create author profile.
